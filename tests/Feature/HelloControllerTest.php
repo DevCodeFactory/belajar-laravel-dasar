792bd400685b13a3beab08e021abe9a4a25f7902
azdyf <azdy.fm@google.com>
2025-06-26 16:49:51 +0700

    Controller

---
+++ tests/Feature/HelloControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HelloControllerTest extends TestCase
+{
+    public function testController()
+    {
+        $this->get('/controller/hello/Fahmi')
+            ->assertStatus(200)
+            ->assertSeeText('Halo Fahmi');
+    }
+
+}
